351e0dc2b6d809711fee3cca1ef1d6ac96ee6de4
n1rozov <nrozov1805@gmail.com>
2022-11-27 21:40:19 +0200

    Contact Update 2

--- src/Body/Contact.js
+++ src/Body/Contact.js
@@ -1,28 +1,29 @@
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import CardFoto from '../CardPhoto.jpg';
+import CardPhoto from '../CardPhoto.jpg';
+import './Contact.scss'
 
 function ContantComponent() {
-    return(
+    return (
         <>
-        <Link to="/contact/school" className="btn btn-secondary stretched-link">School Contacts</Link>
-        <Card className="position-absolute top-50 start-50 translate-middle mt-1" style={{ width: '25rem' }}>
-      <Card.Img variant="top" src={CardFoto} />
-      <Card.Body>
-        <Card.Title className="text-center">Nikita Rozov</Card.Title>
-        <Card.Text className="text-center">
-        I have passed front-end academy in Gamma Intelligence OÜ
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flushc text-center">
-        <ListGroup.Item>My phone number: 56828562</ListGroup.Item>
-        <ListGroup.Item>My Email: nrozov1805@gmail.com</ListGroup.Item>
-      </ListGroup>
-      <Card.Body className="text-center ">
-        <Card.Link href="https://github.com/n1rozov"  className="btn btn-secondary stretched-link">My github link</Card.Link>
-      </Card.Body>
-    </Card>
+            <Link to="/contact/school" className="btn btn-secondary stretched-link">Gamma Intelligence</Link>
+            <Card className="position-absolute top-50 start-50 translate-middle mt-1" style={{ width: '30rem' }}>
+                <Card.Img variant="top" src={CardPhoto} />
+                <Card.Body>
+                    <Card.Title className="text-center">Nikita Rozov</Card.Title>
+                    <Card.Text className="text-center">
+                        I have passed front-end academy in Gamma Intelligence OÜ
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="text-center">
+                    <ListGroup.Item>My Email: nrozov1805@gmail.com</ListGroup.Item>
+                    <ListGroup.Item>My phone number: 56828562</ListGroup.Item>
+                </ListGroup>
+                <Card.Body className="text-center ">
+                    <Card.Link href="https://github.com/n1rozov" className="btn btn-dark stretched-link">My github link</Card.Link>
+                </Card.Body>
+            </Card>
         </>
     )
 };
--- src/Body/ContactSchool.js
+++ src/Body/ContactSchool.js
@@ -11,18 +11,20 @@ function ContantSchoolComponent() {
             <Card className="position-absolute top-50 start-50 translate-middle mt-2" style={{ width: '30rem' }}>
                 <Card.Img variant="top" src={GammaLogo} />
                 <Card.Body>
-                    <Card.Title className="text-center">Gamma Intelligence OÜ</Card.Title>
+                    <Card.Title className="text-center">GAMMA INTELLIGENCE OÜ</Card.Title>
                     <Card.Text className="text-center">
                         Gamma Intelligence OÜ on Eesti ettevõte, mis aitab kiiresti arendada välja uusi tooteid, kasutades lai valiku võimalikedest teholoogiatest (prototüüpeerimine, komponendide valik, tarkvara arendus, testimine, prototüüpi kiirvalmistamine). Samuti pakume IT-konsultatsiooni ja koolitusi
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flushc text-center ">
                     <ListGroup.Item>Email: info@gammatest.net</ListGroup.Item>
-                    <ListGroup.Item>Address: 10145, Tornimäe 7-170, Tallinn, Estonia</ListGroup.Item>
-                    <ListGroup.Item>Phone: +372 555 81 521</ListGroup.Item>
+                    <ListGroup.Item>Aadress: 10145, Tornimäe 7-170, Tallinn, Estonia</ListGroup.Item>
+                    <ListGroup.Item>Tel: +372 555 81 521</ListGroup.Item>
                 </ListGroup>
                 <Card.Body className="text-center ">
-                    <Card.Link href="https://www.gammatest.net/index.php" className="btn btn-dark stretched-link">Link to the Gamma Int. OÜ</Card.Link>
+                    <Card.Link href="https://www.gammatest.net/index.php" className="btn btn-dark">Gamma Int. OÜ koduleht</Card.Link><br></br>
+                    <Card.Link href="https://www.inforegister.ee/11122396-GAMMA-INTELLIGENCE-OU" className="btn btn-dark mt-2">Eesti Inforegister</Card.Link>
+                    <Card.Link href="https://www.facebook.com/GammaIntelligenceTraining/" className="btn btn-dark mt-2">Gamma FaceBook</Card.Link>
                 </Card.Body>
             </Card>
         </>
